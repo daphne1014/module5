98a693ef12103f23c5a607d3770a184596b57101
Daphne Song <85268906+daphne1014@users.noreply.github.com>
2021-07-17 19:56:12 -0700

    complete code

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,40 +8,68 @@ let currentHour = moment().hour(); //Number from 0 to 23
 let startTime = 9;
 let workingHours = 8;
 
-for (i = 0; i < workingHours.length; i++) {
+//create list
+let day = localStorage.getItem('date') || "";
+
+if (day === currentDay) {
+    list = JSON.parse(localStorage.getItem('hourSchedule'));
+} else {
+    list = [];
+    for (i = 0; i < workingHours.length; i++) {
+        list.push('');
+    }
+    localStorage.setItem('hourSchedule', JSON.stringify(list));
+};
+
+for (i = 0; i < workingHours; i++) {
     //create container for each hour as a bootstrap row
     let hourContainer = $('<div>')
         .addClass('row');
-        if ((i + startTime) < currentHour) {
-            hourContainer.addClass('past')
-        } else if ((i + startTime) === currentHour) {
-            hourContainer.addClass('present')
-        } else { (i + startTime) > currentHour } {
-            hourContainer.addClass('future')
-        }
 
     //display time as a partial colum with width of 1
     let time = $('<div>')
         .addClass('col-1')
-        .append(moment(i + startTime).format('h')); //need to check once more
+        .append(moment(i + startTime, ["H"]).format('H')); //need to check once more
     hourContainer.append(time);
 
     //create schedule info inside the container
-    let scheduleP = $('<p>')
+    let scheduleP = $('<textarea>')
         .addClass('col-10')
-        .text(scheduleText);
-  
+        .attr('id', 'text' + i)
+    if ((i + startTime) < currentHour) {
+        scheduleP.addClass('past')
+    } else if ((i + startTime) === currentHour) {
+        scheduleP.addClass('present')
+    } else {
+        scheduleP.addClass('future')
+    }
+
+    scheduleP.text(list[i]);
     hourContainer.append(scheduleP);
 
     //create save button inside the container
 
     let saveBtn = $('<button>')
-        .addClass('col-1 saveBtn');
-    let saveIcon = $('<span>')
-        .addClass("bi bi-save");
+        .addClass('col-1 saveBtn')
+        .attr('schedule-data', i);
+
+    let saveIcon = $('<i>')
+        .addClass("far fa-save");
     saveBtn.append(saveIcon);
 
+
+    //save to local storage
+    saveBtn.on('click', function (event) {
+        event.preventDefault();
+        let now = $(this).attr('schedule-data');
+        list[now] = $('#text' + now).val();
+
+        localStorage.setItem('hourSchedule', JSON.stringify(list));
+        localStorage.setItem('date', currentDay)
+    })
+
     hourContainer.append(saveBtn);
+
 
     //append the rows
     $('#container').append(hourContainer);
